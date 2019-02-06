427815f9888c8f72c51f408306a384807ad7406f
tohartsadok <47325938+tohartsadok@users.noreply.github.com>
2019-02-06 10:51:36 -0500

    Create Template.java
    Created the template that should be renamed if altered for other purposes.

---
+++ Template.java
@@ -0,0 +1,11 @@
+/** This file is meant to be used as a starting point for other projects. 
+class Tentative {
+
+  public static void main(String[] args) {
+  
+    System.out.println("This should be working!");
+    
+  } // end of main
+    
+} // end of class
+    
